306e8fcad22c5f30b9adf8fa86f8a0f0c9a4f91b
peter-mwaura <peterkaranjagitz@gmail.com>
2025-07-04 07:45:17 +0300

    refactor: remove unused styles and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import React from 'react';
 
-function App() {
-    return (
-        <>
-            <Navbar />
-            HomePage
-        </>
-    );
-}
+const App = () => {
+    return <div>App</div>;
+};
 
 export default App;
