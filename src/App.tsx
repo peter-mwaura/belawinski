7a60f15945c2a55c2de9f4ff8090abdcc95d726c
peter-mwaura <peterkaranjagitz@gmail.com>
2025-07-04 13:14:25 +0300

    feat(Navbar): add new navlinks and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import SplitText from 'gsap/SplitText';
+import Navbar from './components/Navbar';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <main>
+            <Navbar />
+        </main>
+    );
 };
 
 export default App;
